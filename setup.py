bb702e2bbddc37335560bedf95fe6f089ec03a4b
Colin Barnwell <cbarnwell@twig-it.com>
2014-01-24 15:29:06 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='django-crosssubdomain',
-    version='0.1',
+    version='0.1.1',
     author='Colin Barnwell',
     packages=find_packages(),
     package_data={},
